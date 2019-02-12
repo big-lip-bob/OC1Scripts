3144f12ef711e9cc277c7f63bc2b6490b9cfff94
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2019-02-12 13:01:59 +0100

    Update Fission.lua

--- Scripts/NuclearCraft/Fission.lua
+++ Scripts/NuclearCraft/Fission.lua
@@ -1,23 +1,13 @@
 local component = require("component")
 local term = require("term")
-local thread = require("thread")
+local event = require("event")
 
 if not component.isAvailable("nc_fission_reactor") then
   print("Reactor not connected. Please connect the computer to the fission reactor.")
   os.exit()
 end
 
-function exit(msg)
-term.clear()
-print(msg)
-os.exit()
-end
-
-t = thread.create(function()
-  os.sleep(3)
-  event.pull("key_")
-  exit("Exiting due to keyboard press")
-end)
+local run = true
 
 os.execute("resolution 48 24")
 
@@ -27,53 +17,65 @@ local SEP = 0
 local MHP = 50
 local WSEP = 75
 
-while t:status() ~= "dead" do
+if term.isAvailable() then
+	local E = (math.floor(reactor.getEfficiency() * 10)) / 10
+	local H = (math.floor(reactor.getHeatMultiplier() * 10)) / 10
+	local C = math.floor(math.abs(reactor.getEnergyChange()))
+	local preG = math.floor(reactor.getReactorProcessHeat())
+	local G
+	if preG > 0
+	then G = preG
+	else G = "None"
+	end
+end
+	
+while run do
 
   	local SEP = reactor.getEnergyStored() / reactor.getMaxEnergyStored() * 100
 	local HP = reactor.getHeatLevel() / reactor.getMaxHeatLevel() * 100
 	
     if HP > MHP or SEP > WSEP
-		then reactor.deactivate()
-		else reactor.activate()
+	 then reactor.deactivate()
+	 else reactor.activate()
     end
 	
   if term.isAvailable() then
-		local E = (math.floor(reactor.getEfficiency() * 10)) / 10
-		local H = (math.floor(reactor.getHeatMultiplier() * 10)) / 10
-		local C = math.floor(math.abs(reactor.getEnergyChange()))
-		
-		local A
-		if C > 0
-		then A = "Active"
-		else A = "Standby"
-		end
-		
-		local L = {
-		"" ,
-		"" .. math.floor(reactor.getLengthX()) .. "x" .. math.floor(reactor.getLengthY()) .. "x" .. math.floor(reactor.getLengthZ()) .. " " .. A .. " Fission Reactor" ,
-		"Fuel :       " .. reactor.getFissionFuelName() ,
-		"             " .. reactor.getFissionFuelPower() .. " RF/t - " .. reactor.getFissionFuelHeat() .. " H/t",
-		"Efficiency : " .. E .. "%" ,
-		"Heat :       " .. H .. "%" ,
-		"Energy :     " .. math.floor(reactor.getEnergyStored()) .. " / " .. math.floor(reactor.getMaxEnergyStored()) ,
-		"Energy I/O : " .. C ,
-		"Heat :       " .. math.floor(reactor.getHeatLevel()) .. " / " .. math.floor(reactor.getMaxHeatLevel()) ,
-		"Heat Gen :   " .. math.floor(reactor.getReactorProcessHeat())
-		}
-		
-		term.clear()
-		
-		local LM = 3
-		local S = 1
-		
-	for i, v in ipairs(L) do
-      term.setCursor(LM , ( i * S ))
-      term.write(v)
-    end
+	local A = ""
+	if C > 0
+	then A = "Active"
+	else A = "Standby"
+	end
+	
+	local L = {
+	"" ,
+	"" .. math.floor(reactor.getLengthX()) .. "x" .. math.floor(reactor.getLengthY()) .. "x" .. math.floor(reactor.getLengthZ()) .. " " .. A .. " Fission Reactor" ,
+	"Fuel :       " .. reactor.getFissionFuelName() ,
+	"             " .. reactor.getFissionFuelPower() .. " RF/t - " .. reactor.getFissionFuelHeat() .. " H/t",
+	"Efficiency : " .. E .. "%" ,
+	"Heat :       " .. H .. "%" ,
+	"Energy :     " .. math.floor(reactor.getEnergyStored()) .. " / " .. math.floor(reactor.getMaxEnergyStored()) ,
+	"Energy I/O : " .. C ,
+	"Heat :       " .. math.floor(reactor.getHeatLevel()) .. " / " .. math.floor(reactor.getMaxHeatLevel()) ,
+	"Heat Gen :   " .. G
+	}
 
+	term.clear()
+
+	local LM = 3
+	local S = 1
+		
+   for i, v in ipairs(L) do
+    term.setCursor(LM , ( i * S ))
+    term.write(v)
+   end
   end
   
-  os.sleep(0.25)
+  local event, addres , v1 , v2 = event.pull(0.25)
+  if event == "key_down" then
+   term.clear()
+   print("Exiting due to keyboard press")
+   break
+  end
   
 end
 
