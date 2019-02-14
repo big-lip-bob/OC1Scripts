ee5784953d956ee0a404502695ab1b4337865289
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2019-02-14 20:20:55 +0100

    Update Fission.lua

--- Scripts/NuclearCraft/Fission.lua
+++ Scripts/NuclearCraft/Fission.lua
@@ -7,16 +7,11 @@ if not component.isAvailable("nc_fission_reactor") then
   os.exit()
 end
 
-local run = true
-
 local x , y = component.gpu.maxResolution()
 local x = math.min(y,24) * 2
 component.gpu.setResolution(x,x/2)
 
-local reactor = component.nc_fission_reactor
-
-local E,H,C,G,HP,SEP,MPH,WSEP
-local runv = true
+local reactor,MPH,WSEP,runv,help,auto,E,H,C,G = component.nc_fission_reactor,50,75,true,true,false
 
 function run(b)
  if b and runv then return true
@@ -24,32 +19,44 @@ function run(b)
  end
 end
 
-function update()
- HP = 0
- SEP = 0
- MHP = 50
- WSEP = 75
- 
- if term.isAvailable() then
-  E = (math.floor(reactor.getEfficiency() * 10)) / 10
-  H = (math.floor(reactor.getHeatMultiplier() * 10)) / 10
-  C = math.floor(math.abs(reactor.getEnergyChange()))
-  local preG = math.floor(reactor.getReactorProcessHeat())
-  if preG > 0
-   then G = preG
-   else G = "None"
-  end
- end
+function runf() run(false) end
+function updatef() reactor.forceUpdate() end
+function autof() if auto == true then auto = false else auto = true end end
+function helpf() if help == true then help = false else help = true end end
+
+local keyactions = {[35]=helpf,[30]=autof,[22]=updatef,[18]=runf}
+
+local preG = math.floor(reactor.getReactorProcessHeat())
+if preG > 0
+  then G = preG
+ else G = "None"
 end
 
-update()
-
+while run(true) do
+ 
+ if auto == true then
+  SEP = reactor.getEnergyStored() / reactor.getMaxEnergyStored() * 100
+  HP = reactor.getHeatLevel() / reactor.getMaxHeatLevel() * 100
+  if HP > MHP or SEP > WSEP
+   then reactor.deactivate()
+   else reactor.activate()
+  end
+ end
+ 
+  if term.isAvailable() then
+  
+   E = (math.floor(reactor.getEfficiency() * 10)) / 10
+   H = (math.floor(reactor.getHeatMultiplier() * 10)) / 10
+   C = math.floor(math.abs(reactor.getEnergyChange()))
+   
+  if G ~= "None" then G = math.floor(reactor.getReactorProcessHeat()) end
+  
   local A = ""
-  if C > 0
+  if reactor.isProcessing()
    then A = "Active"
    else A = "Standby"
   end
-
+  
 local L = {
 "" ,
 "" .. math.floor(reactor.getLengthX()) .. "x" .. math.floor(reactor.getLengthY()) .. "x" .. math.floor(reactor.getLengthZ()) .. " " .. A .. " Fission Reactor" ,
@@ -63,8 +70,6 @@ local L = {
 "Heat Gen :   " .. G,
 }
 
-local lent = #L
-
 local Help = {
 "",
 "Press U to call an update to all the values",
@@ -72,45 +77,25 @@ local Help = {
 "Press E to exit the script",
 "Press H to toggle this help message"
 }
-
-while run(true) do
-
- local auto = false
- 
- if auto == true then
-  local SEP = reactor.getEnergyStored() / reactor.getMaxEnergyStored() * 100
-  local HP = reactor.getHeatLevel() / reactor.getMaxHeatLevel() * 100
-   if HP > MHP or SEP > WSEP
-   then reactor.deactivate()
-   else reactor.activate()
-  end
- end
-	
- if term.isAvailable() then
-	
-  local help = true
-
+		
   term.clear()
-
-  local LM = 3
 		
   for i, v in ipairs(L) do
-   term.setCursor(LM,i)
+   term.setCursor(3,i)
    term.write(v)
   end
    
   if help == true then
    for i,v in ipairs(Help) do
-    term.setCursor(LM,lent+i)
+    term.setCursor(3,10+i)
     term.write(v)
    end
   end
-  local function autof() if auto == true then auto = false else auto = true end end
-  local function helpf() if help == true then help = false else help = true end end
+  
   local event, addres , v1 , v2 = event.pull(0.3)
-  local keyactions = {[35]=helpf,[30]=autof,[22]=update,[18]=run(false)}
-  if event == "key_down" then if keyactions[v2] ~= nil then keyactions[v2]() end end
- else auto = true
+  if event == "key_down" then
+   if keyactions[v2] ~= nil then keyactions[v2]() end
+  end
  end
 end
 
